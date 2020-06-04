4899804499a7e92afeca2e5fd354e9f5c7ad0e0e
SHorne <swhorne@gmail.com>
2020-06-03 22:54:47 -0600

    Adding Schema, Seeds, Server, Orm and connections

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalpeno Popper Burger');
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Western Burger', true);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('El Jefe Burger', true);
